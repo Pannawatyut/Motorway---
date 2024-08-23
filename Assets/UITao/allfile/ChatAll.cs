0633b2a9c34f55dbd4e623f5d062b8159cc0c281
TaoEiEi <sirichai44.ta@gmail.com>
2024-08-23 17:06:22 +0700

    UI fix 2

--- Assets/UITao/allfile/ChatAll.cs
+++ Assets/UITao/allfile/ChatAll.cs
@@ -13,6 +13,35 @@ public class ChatAll : MonoBehaviour
     private Queue<GameObject> messageQueue = new Queue<GameObject>();
     private string playerNickname;
 
+    // List of profane words (this is just an example, you may want to use a more comprehensive list)
+    private List<string> profaneWords = new List<string>
+    { 
+        // คำหยาบภาษาไทย
+        "ไอ้", "อี", "เหี้ย", "เชี่ย", "แม่ง", "สัส", 
+        "สัด", "มึง", "กู", "ห่า", "ควย", 
+        "เย็ด", "หี", "แตด", "ฟาย", "ตอแหล", "ระยำ", 
+        "สัตว์", "ดอกทอง", "จัญไร", "ชาติชั่ว", "เห้", 
+        "พ่อง", "แม่ม", "สถุน", "กาก", "สวะ", "หน้าตัวเมีย",
+
+        // คำหยาบภาษาอังกฤษ
+        "fuck", "shit", "bitch", "asshole", "bastard", "dick", 
+        "pussy", "cunt", "motherfucker", "nigger", "fag", 
+        "slut", "whore", "douche", "cock", "jerk", "freak", 
+        "suck", "crap", "dumbass", "retard", "idiot", "moron",
+
+        // รูปแบบ Leet ภาษาไทย
+        "คuย", "ค_ย", "ควe", "ค.วย",
+        "hี", "ฮี",
+        "เ_้ย", "เ_ี้ย", "เ-้ย", "เ-ี้ย",
+        "สaส", "สั_ส", "สัต", "สัตว์",
+        "เ_ด", "เ-ด", "เ_ย็ด", "เย็d",
+
+        // รูปแบบ Leet ภาษาอังกฤษ
+        "f@ck", "sh!t", "b!tch", "4ss", "b@stard", "d!ck", 
+        "p@ssy", "c*nt", "m0therfucker", "n!gger", "f@g", 
+        "sl*t", "wh0re", "d0uche", "c*ck", "j3rk", "fr3ak", 
+        "suck", "cr@p", "dumb@ss", "r3tard", "idi0t", "m0r0n"
+    }; 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Return))
@@ -23,7 +52,12 @@ public class ChatAll : MonoBehaviour
 
     public void SendMessage()
     {
-        if (string.IsNullOrWhiteSpace(inputField.text)) return;
+        string messageText = inputField.text;
+
+        if (string.IsNullOrWhiteSpace(messageText)) return;
+
+        // Replace profane words with ****
+        messageText = ReplaceProfaneWords(messageText);
 
         playerNickname = PhotonNetwork.LocalPlayer.NickName;
         if (string.IsNullOrEmpty(playerNickname))
@@ -31,8 +65,18 @@ public class ChatAll : MonoBehaviour
             playerNickname = "UnknownPlayer"; // Default nickname if none is set
         }
 
-        GetComponent<PhotonView>().RPC("GetMessage", RpcTarget.All, inputField.text, playerNickname);
+        GetComponent<PhotonView>().RPC("GetMessage", RpcTarget.All, messageText, playerNickname);
         inputField.text = ""; // Clear input field after sending
+    }
+
+    private string ReplaceProfaneWords(string message)
+    {
+        foreach (var word in profaneWords)
+        {
+            string replacement = new string('*', word.Length);
+            message = message.Replace(word, replacement, System.StringComparison.OrdinalIgnoreCase);
+        }
+        return message;
     }
 
     [PunRPC]
---
+++ Assets/UpdateProfile.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class UpdateProfile : MonoBehaviour
+{
+    public string firstName = "sirichai";
+    public string lastName = "Tang";
+    public string gender = "ชาย";
+    public string age = "20-30 ปี";
+    public string education = "ปริญาเอก";
+    public string occupation = "นักศึกษา";
+    public string vehicle = "รถยนต์ส่วนตัว";
+    public string checkpoint = "ทุกวัน";
+
+    public void OnUpdateButtonClicked()
+    {
+        StartCoroutine(UpdateUser());
+    }
+
+    private IEnumerator UpdateUser()
+    {
+        var updateData = new UpdateUserRequest
+        {
+            account = new AccountUpdate
+            {
+                first_name = firstName,
+                last_name = lastName,
+                gender = gender,
+                age = age,
+                education = education,
+                occupation = occupation,
+                vehicle = vehicle,
+                checkpoint = checkpoint
+            }
+        };
+
+        string json = JsonUtility.ToJson(updateData);
+
+        // ตรวจสอบ JSON payload ที่จะส่ง
+        Debug.Log("JSON Payload: " + json);
+
+        using var request = new UnityWebRequest("http://13.250.106.216:1000/api/user/updateProfile", "POST")
+        {
+            uploadHandler = new UploadHandlerRaw(System.Text.Encoding.UTF8.GetBytes(json)),
+            downloadHandler = new DownloadHandlerBuffer()
+        };
+
+        request.SetRequestHeader("Content-Type", "application/json");
+
+        // ส่งคำขอและรอรับการตอบกลับ
+        yield return request.SendWebRequest();
+
+        // ตรวจสอบผลลัพธ์ของคำขอ
+        if (request.result != UnityWebRequest.Result.Success)
+        {
+            Debug.LogError("Error: " + request.error);
+        }
+        else
+        {
+            Debug.Log("Response Code: " + request.responseCode);
+            Debug.Log("Response Text: " + request.downloadHandler.text);
+
+            // แสดงผลลัพธ์ในกรณีที่คำขอสำเร็จ
+            if (request.responseCode == 200)
+            {
+                Debug.Log("Update successful!");
+            }
+            else
+            {
+                Debug.LogError("Update failed with response code: " + request.responseCode);
+            }
+        }
+    }
+}
+
+[System.Serializable]
+public class UpdateUserRequest
+{
+    public AccountUpdate account;
+}
+
+[System.Serializable]
+public class AccountUpdate
+{
+    public string first_name;
+    public string last_name;
+    public string gender;
+    public string age;
+    public string education;
+    public string occupation;
+    public string vehicle;
+    public string checkpoint;
+}
