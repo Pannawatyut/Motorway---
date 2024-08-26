18ee1f8797aa308379200aaef6ef37a9981fe16b
Pannawatyut <153474480+Pannawatyut@users.noreply.github.com>
2024-08-26 18:02:32 +0700

    GoogleSignin

---
+++ Assets/Script/playGamesManager.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using Google;
+using System.Threading.Tasks;
+using UnityEngine.Networking;
+
+public class playGamesManager : MonoBehaviour
+{
+
+    private GoogleSignInConfiguration configuration;
+    public string webClientId = "819459652226-a9l99jtljkrse17mbmn2lgpcp0ktbcvo.apps.googleusercontent.com";
+
+
+
+    void Awake()
+    {
+        configuration = new GoogleSignInConfiguration
+        {
+            WebClientId = webClientId,
+            RequestIdToken = true,
+            UseGameSignIn = false,
+            RequestEmail = true
+        };
+    }
+
+    public void OnSignIn()
+    {
+        GoogleSignIn.Configuration = configuration;
+        GoogleSignIn.DefaultInstance.SignIn().ContinueWith(
+            OnAuthenticationFinished, TaskScheduler.Default);
+    }
+
+    internal void OnAuthenticationFinished(Task<GoogleSignInUser> task)
+    {
+        if (task.IsFaulted)
+        {
+            using (IEnumerator<System.Exception> enumerator =
+                task.Exception.InnerExceptions.GetEnumerator())
+            {
+                if (enumerator.MoveNext())
+                {
+                    GoogleSignIn.SignInException error =
+                        (GoogleSignIn.SignInException)enumerator.Current;
+                    Debug.LogError("Got Error: " + error.Status + " " + error.Message);
+                }
+                else
+                {
+                    Debug.LogError("Got unexpected exception?!?" +  task.Exception);
+                }
+            }
+        }
+        else if (task.IsCanceled)
+        {
+            Debug.LogError("Cancelled");
+        }
+        else
+        {
+        }
+    }
+
+ 
+
+    public void OnSignOut()
+    {
+       
+        Debug.Log("Calling SignOut");
+        GoogleSignIn.DefaultInstance.SignOut();
+    }
+
+}
