670240a663ce8e97a15a2dc41d02023a53ddb6ea
Pannawatyut <153474480+Pannawatyut@users.noreply.github.com>
2024-08-27 13:14:38 +0700

    FacebookSDK Importing

--- Assets/Script/playGamesManager.cs
+++ Assets/Script/playGamesManager.cs
@@ -1,67 +1,67 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using GooglePlayGames;
-using GooglePlayGames.BasicApi;
-using UnityEngine.SocialPlatforms;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using GooglePlayGames;
+// using GooglePlayGames.BasicApi;
+// using UnityEngine.SocialPlatforms;
 
-public class PlayGamesManager : MonoBehaviour
-{
-    void Awake()
-    {
-        // Configure Google Play Games
-        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder()
-            .RequestEmail()
-            .Build();
+// public class PlayGamesManager : MonoBehaviour
+// {
+//     void Awake()
+//     {
+//         // Configure Google Play Games
+//         PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder()
+//             .RequestEmail()
+//             .Build();
 
-        PlayGamesPlatform.InitializeInstance(config);
-        PlayGamesPlatform.Activate();
+//         PlayGamesPlatform.InitializeInstance(config);
+//         PlayGamesPlatform.Activate();
 
-        // Sign in at the start
-        SignIn();
-    }
+//         // Sign in at the start
+//         SignIn();
+//     }
 
-    public void SignIn()
-    {
-        Social.localUser.Authenticate((bool success) => {
-            if (success)
-            {
-                Debug.Log("Signed in! Welcome " + Social.localUser.userName);
-            }
-            else
-            {
-                Debug.LogError("Failed to sign in.");
-            }
-        });
-    }
+//     public void SignIn()
+//     {
+//         Social.localUser.Authenticate((bool success) => {
+//             if (success)
+//             {
+//                 Debug.Log("Signed in! Welcome " + Social.localUser.userName);
+//             }
+//             else
+//             {
+//                 Debug.LogError("Failed to sign in.");
+//             }
+//         });
+//     }
 
-    public void SignOut()
-    {
-        PlayGamesPlatform.Instance.SignOut();
-        Debug.Log("User signed out.");
-    }
+//     public void SignOut()
+//     {
+//         PlayGamesPlatform.Instance.SignOut();
+//         Debug.Log("User signed out.");
+//     }
 
-    public void ShowLeaderboard()
-    {
-        if (Social.localUser.authenticated)
-        {
-            PlayGamesPlatform.Instance.ShowLeaderboardUI();
-        }
-        else
-        {
-            Debug.LogError("User not authenticated.");
-        }
-    }
+//     public void ShowLeaderboard()
+//     {
+//         if (Social.localUser.authenticated)
+//         {
+//             PlayGamesPlatform.Instance.ShowLeaderboardUI();
+//         }
+//         else
+//         {
+//             Debug.LogError("User not authenticated.");
+//         }
+//     }
 
-    public void ShowAchievements()
-    {
-        if (Social.localUser.authenticated)
-        {
-            PlayGamesPlatform.Instance.ShowAchievementsUI();
-        }
-        else
-        {
-            Debug.LogError("User not authenticated.");
-        }
-    }
-}
+//     public void ShowAchievements()
+//     {
+//         if (Social.localUser.authenticated)
+//         {
+//             PlayGamesPlatform.Instance.ShowAchievementsUI();
+//         }
+//         else
+//         {
+//             Debug.LogError("User not authenticated.");
+//         }
+//     }
+// }
