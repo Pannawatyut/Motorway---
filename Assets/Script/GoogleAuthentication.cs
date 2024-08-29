f684c4a49f144f41064e8ac765241b258da71698
Pannawatyut <153474480+Pannawatyut@users.noreply.github.com>
2024-08-29 10:05:07 +0700

    PushLoginWEBGL_Finished

--- Assets/Script/GoogleAuthentication.cs
+++ Assets/Script/GoogleAuthentication.cs
@@ -1,69 +1,69 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using TMPro;
-using UnityEngine.UI;
-using Google;
-using System.Threading.Tasks;
-using UnityEngine.Networking;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using TMPro;
+// using UnityEngine.UI;
+// using Google;
+// using System.Threading.Tasks;
+// using UnityEngine.Networking;
 
-public class GoogleAuthentication : MonoBehaviour
-{
+// public class GoogleAuthentication : MonoBehaviour
+// {
 
-    private GoogleSignInConfiguration configuration;
-    public string webClientId = "819459652226-a9l99jtljkrse17mbmn2lgpcp0ktbcvo.apps.googleusercontent.com";
+//     private GoogleSignInConfiguration configuration;
+//     public string webClientId = "819459652226-a9l99jtljkrse17mbmn2lgpcp0ktbcvo.apps.googleusercontent.com";
 
 
 
-    void Awake()
-    {
-        configuration = new GoogleSignInConfiguration
-        {
-            WebClientId = webClientId,
-            RequestIdToken = true,
-            UseGameSignIn = false,
-            RequestEmail = true
-        };
-    }
+//     void Awake()
+//     {
+//         configuration = new GoogleSignInConfiguration
+//         {
+//             WebClientId = webClientId,
+//             RequestIdToken = true,
+//             UseGameSignIn = false,
+//             RequestEmail = true
+//         };
+//     }
 
-    public void OnSignIn()
-    {
-        GoogleSignIn.Configuration = configuration;
-        GoogleSignIn.DefaultInstance.SignIn().ContinueWith(
-            OnAuthenticationFinished, TaskScheduler.Default);
-    }
+//     public void OnSignIn()
+//     {
+//         GoogleSignIn.Configuration = configuration;
+//         GoogleSignIn.DefaultInstance.SignIn().ContinueWith(
+//             OnAuthenticationFinished, TaskScheduler.Default);
+//     }
 
-    internal void OnAuthenticationFinished(Task<GoogleSignInUser> task)
-    {
-        if (task.IsFaulted)
-        {
-            using (IEnumerator<System.Exception> enumerator =
-                task.Exception.InnerExceptions.GetEnumerator())
-            {
-                if (enumerator.MoveNext())
-                {
-                    GoogleSignIn.SignInException error =
-                        (GoogleSignIn.SignInException)enumerator.Current;
-                    Debug.LogError("Got Error: " + error.Status + " " + error.Message);
-                }
-                else
-                {
-                    Debug.LogError("Got unexpected exception?!?" + task.Exception);
-                }
-            }
-        }
-        else if (task.IsCanceled)
-        {
-            Debug.LogError("Cancelled");
-        }
+//     internal void OnAuthenticationFinished(Task<GoogleSignInUser> task)
+//     {
+//         if (task.IsFaulted)
+//         {
+//             using (IEnumerator<System.Exception> enumerator =
+//                 task.Exception.InnerExceptions.GetEnumerator())
+//             {
+//                 if (enumerator.MoveNext())
+//                 {
+//                     GoogleSignIn.SignInException error =
+//                         (GoogleSignIn.SignInException)enumerator.Current;
+//                     Debug.LogError("Got Error: " + error.Status + " " + error.Message);
+//                 }
+//                 else
+//                 {
+//                     Debug.LogError("Got unexpected exception?!?" + task.Exception);
+//                 }
+//             }
+//         }
+//         else if (task.IsCanceled)
+//         {
+//             Debug.LogError("Cancelled");
+//         }
 
-    }
+//     }
 
-    public void OnSignOut()
-    {
+//     public void OnSignOut()
+//     {
 
-        Debug.Log("Calling SignOut");
-        GoogleSignIn.DefaultInstance.SignOut();
-    }
+//         Debug.Log("Calling SignOut");
+//         GoogleSignIn.DefaultInstance.SignOut();
+//     }
 
-}
+// }
